9a5ffa29359c944558fc5a45d5a2beb5257e1605
LeQuang050698 <quanglm050698@gmail.com>
2023-08-08 00:04:16 +0700

    fix

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,3 +1,4 @@
+using System.Dynamic;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,48 +10,64 @@ namespace MyApp
     {
         static void Main(string[] args) 
         {
-            Console.WriteLine("Please Choose: \n 1: Triangle \n 2: Rectangle");
-            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("1: Rectangle\n2: Triangle");
+            int number;
+            do {
+                Console.WriteLine("Your Choice:");
+                number = Convert.ToInt32(Console.ReadLine());
+                if(number == 1) {
+                    SetRectangle();
+                } else if(number == 2) {
+                    SetTriangle(); 
+                }
+            }while(number != 1 && number != 2);
+        }
 
-            if(number == 1) {
-                Console.Write("XY: ");
-                float xy = Convert.ToSingle(Console.ReadLine());
+        static void SetRectangle() {
+            float lengthRec, widthRec;
+            do {
+                Console.WriteLine("Length Rectangle: ");
+                lengthRec = Convert.ToSingle(Console.ReadLine());
 
-                Console.Write("YZ: ");
-                float yz = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Width Rectangle: ");
+                widthRec = Convert.ToSingle(Console.ReadLine());
 
-                Console.Write("ZX: ");
-                float xz = Convert.ToSingle(Console.ReadLine());
-                if(xy > 0 && yz > 0 && xz > 0) {
-                    if(xy >= yz + xz || yz >= xz + xy || xz >= xy + yz) {
-                        Console.WriteLine("This is a not Triangle");
+                if(lengthRec > 0 && widthRec > 0) {
+                    Rectangle abcd = new Rectangle(lengthRec, widthRec);
+                    abcd.PRectangle();
+                    abcd.SRectangle();
+                }else {
+                    Console.WriteLine("Please Re-Enter!");
+                }
+            }while(lengthRec <= 0 || widthRec <= 0);
+        }
+
+        static void SetTriangle() {
+            float firstEdge, secondEdge, thirdEdge;
+            do {
+                Console.WriteLine("The First Side Of The Triangle: ");
+                firstEdge = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("The Second Side Of The Triangle: ");
+                secondEdge = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("The Third Side Of The Triangle: ");
+                thirdEdge = Convert.ToSingle(Console.ReadLine());
+
+                if(firstEdge > 0 && secondEdge > 0 && thirdEdge > 0) {
+                    if(firstEdge >= secondEdge + thirdEdge || secondEdge >= firstEdge + thirdEdge || thirdEdge >= firstEdge + secondEdge) {
+                        Console.WriteLine("This is not a triangle");
                     } else {
-                        Triangle xyz = new Triangle(xy, yz, xz);
+                        Triangle xyz = new Triangle(firstEdge, secondEdge, thirdEdge);
                         xyz.PTriangle();
                         xyz.STriangle();
                     }
-                } else {
-                    Console.WriteLine("Please Re-Enter");
+                }else {
+                    Console.WriteLine("Please Re-Enter!");
                 }
-            } else if(number == 2) {
-                Console.Write("Length Rectangle: ");
-                float lr = Convert.ToSingle(Console.ReadLine());
-
-                Console.Write("Width Rectangle: ");
-                float wr = Convert.ToSingle(Console.ReadLine());
-
-                if(lr > 0 && wr > 0) {
-                    Rectangle abcd = new Rectangle(lr, wr);
-                    abcd.PRectangle();
-                    abcd.SRectangle();
-                } else {
-                    Console.WriteLine("Please Re-Enter");
-
-                }
-            } else {
-                Console.WriteLine("Please Re-Enter");
-            }
+            }while (firstEdge <= 0 || secondEdge <= 0 || thirdEdge <= 0);
         }
+        
     }
 
 }
