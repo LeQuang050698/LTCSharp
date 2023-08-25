b3ad3cab0439c66c555312221837c212eb253dd7
LeQuang050698 <quanglm050698@gmail.com>
2023-08-25 17:28:59 +0700

    fix

--- Loop/CharacterCount/CharacterCount.cs
+++ Loop/CharacterCount/CharacterCount.cs
@@ -8,38 +8,24 @@ namespace CharacterCount
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             Console.Write("Enter String: ");
-            string myString = Console.ReadLine();
-            myString = myString.Replace(" ", string.Empty);
-            myString = myString.Trim();
-            while(myString.Length > 0)
-            {
-                Console.Write(myString[0] + ": ");
-                int count = 0;
-
-                for(int i = 0; i < myString.Length; ++i) {
-                    if(myString[0] == myString[i]) {
-                        count++;
+            string str = Console.ReadLine();
+            char[] charArr = str.ToCharArray();
+            int Length = charArr.Length;
+            
+            for(int i = 0; i < Length; i++) {
+                int count = charArr.Count(x => x == charArr[i]);
+                int j;
+                for(j = 0; j < Length; j++) {
+                    if(charArr[i] == charArr[j]) {
+                        break;
                     }
                 }
-                Console.WriteLine(count);
-                myString = myString.Replace(myString[0].ToString(), string.Empty);
+                if(i == j) {
+                    Console.WriteLine($"{charArr[i]}: {count}");
+                }
             }
-
-            // int Length = myString.Length;
-            // for(int i = 0; i < Length; ++i) {
-            //     for(int j = i + 1; j < Length; ++j) {
-            //         if(myString[i] == myString[j]) {
-            //             for(int k = j + 1; k < Length; ++k) {
-            //                 myString[k] = myString[k - 1];
-            //                 i--;
-            //                 Length--;
-            //             }
-            //         }
-            //     }
-            // }
-            // Console.WriteLine(myString);
         }
     }
 }
