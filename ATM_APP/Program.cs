a5d0fa297a61627570160ed20a3d3623f96a4199
LeQuang050698 <quanglm050698@gmail.com>
2023-09-12 11:29:04 +0700

    fix

--- ATM_APP/Program.cs
+++ ATM_APP/Program.cs
@@ -10,7 +10,7 @@ namespace ATM_APP
         static void Main(string[] args)
         {
             long amount = default;
-            var ListUsersId = new Dictionary<long, long>() {{77882664,0},{664444442,0}};
+            var ListUsersId = new Dictionary<long, long>(){{12345678,100000000}};
             AtmUserId user = new AtmUserId();
             ShowMenuLogin(user,ListUsersId,amount);
         }
@@ -28,11 +28,11 @@ namespace ATM_APP
                     case 1:
                         Console.Write("Enter User ID:");
                         currentUser.userId = Convert.ToInt64(Console.ReadLine());
-                        while(!ListUsersId.ContainsKey(currentUser.userId)){
-                            Console.Write("Re-Enter User ID:");
-                            currentUser.userId = Convert.ToInt64(Console.ReadLine());
+                        ListUsersId.Add(currentUser.userId,currentUser.balance);
+
+                        foreach(KeyValuePair<long,long> ListUserId in ListUsersId) {
+                            Console.WriteLine($"{ListUserId.Key}: {ListUserId.Value}");
                         }
-                        ShowMainMenu(currentUser,ListUsersId,amount);
                     break;
                     case 2: Console.WriteLine("Thank You & See You Again");break;
                     default:break;
@@ -51,38 +51,48 @@ namespace ATM_APP
                 Console.Write("Choose:");
                 userChoose = Convert.ToInt32(Console.ReadLine());
                 switch(userChoose){
-                    case 1: currentUser.showBalance();break;
-                    case 2: Withdraw(currentUser, amount);break;
-                    case 3: currentUser.handleDeposite();break;
-                    case 4: ShowMenuLogin(currentUser,ListUsersId,amount);break;
+                    case 1:
+                        currentUser.showBalance();
+                        ShowMainMenu(currentUser,ListUsersId,amount);    
+                    break;
+                    case 2:
+                        Withdraw(currentUser);
+                        ShowMainMenu(currentUser,ListUsersId,amount);    
+                    break;
+                    case 3:
+                        currentUser.handleDeposite();
+                        ShowMainMenu(currentUser,ListUsersId,amount);    
+                    break;
+                    case 4:
+                        ShowMenuLogin(currentUser,ListUsersId,amount);break;
                     default:break;
                 }
             }
         }
-        static void Withdraw(AtmUserId currentUser, long amount)
+        static void Withdraw(AtmUserId currentUser)
         {
             Console.WriteLine("<==========>Withdraw<==========>");
             Console.WriteLine("1.100000\t\t2.200000\t\t3.500000");
             Console.WriteLine("4.1000000\t\t5.2000000\t\t6.5000000");
-            Console.WriteLine("7.1000000\t\t8.Enter Another\t\t9.Back");
+            Console.WriteLine("7.10000000\t\t8.Enter Another\t\t9.Back");
             int userChoose = 0;
             while(!(userChoose > 0 && userChoose < 10)){
                 Console.Write("Choose:");
                 userChoose = Convert.ToInt32(Console.ReadLine());
                 switch(userChoose){
-                    case 1: amount = 100000; break;
-                    case 2: amount = 200000; break;
-                    case 3: amount = 500000; break;
-                    case 4: amount = 1000000; break;
-                    case 5: amount = 2000000; break;
-                    case 6: amount = 5000000; break;
-                    case 7: amount = 10000000; break;
+                    case 1: currentUser.amount = 100000; break;
+                    case 2: currentUser.amount = 200000; break;
+                    case 3: currentUser.amount = 500000; break;
+                    case 4: currentUser.amount = 1000000; break;
+                    case 5: currentUser.amount = 2000000; break;
+                    case 6: currentUser.amount = 5000000; break;
+                    case 7: currentUser.amount = 10000000; break;
                     case 8:
                         Console.Write("Enter Another Money:");
-                        amount = Convert.ToInt64(Console.ReadLine());
-                        while(amount %10000 != 0) {
+                        currentUser.amount = Convert.ToInt64(Console.ReadLine());
+                        while(currentUser.amount %10000 != 0) {
                             Console.Write("Please Re-Enter:");
-                            amount = Convert.ToInt64(Console.ReadLine());
+                            currentUser.amount = Convert.ToInt64(Console.ReadLine());
                         }
                     break;
                 } 
